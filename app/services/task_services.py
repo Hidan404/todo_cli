b0d1f62e308559fff4f51b9e7f8ed5534ebeb9b1
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-03-17 22:37:08 -0300

    refactor: retirar instancia da classe nos parametros dos metodos

--- app/services/task_services.py
+++ app/services/task_services.py
@@ -3,15 +3,16 @@ from  models.task import Task
 
 class TaskServices():
     def criar_tarefa(self, titulo, descricao):
-        self.task = Task(titulo, descricao)
-        self.task.salvar()
+        nova_tarefa = task = Task(titulo, descricao)
+        nova_tarefa.salvar()
+        return nova_tarefa
 
 
     def listar_tarefas(self):
-        return Task.listar(self.task)
+        return Task.listar()
     
     def verificar_se_tabela_existe(self):
-        return Task.verificar_se_tabela_existe(self.task)
+        return Task.verificar_se_tabela_existe()
     
     def deletar_tarefas(self, id):
-        return Task.deletar(self.task, id)
+        return Task.deletar(id)
