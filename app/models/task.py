ec75b5f780516bf65071b4c8197ffcb4866b4be6
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-03-18 22:04:33 -0300

    refactor: Mudar metodos da classe para metodos estaticos

--- app/models/task.py
+++ app/models/task.py
@@ -10,11 +10,14 @@ class Task:
         self.descricao = descricao
         self.data_criacao = data_criacao or datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    def caminho_banco(self):
+    @staticmethod
+    def caminho_banco():
         return Path(__file__).parent.parent / 'database' / 'tarefas.db'
 
-    def criar_tabela(self):
-        conn = sqlite3.connect(self.caminho_banco())
+
+    @staticmethod
+    def criar_tabela():
+        conn = sqlite3.connect(Task.caminho_banco())
         cursor = conn.cursor()
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS tarefas (
@@ -27,19 +30,21 @@ class Task:
         conn.commit()
         conn.close()
 
-    def salvar(self):
-        self.criar_tabela()
-        conn = sqlite3.connect(self.caminho_banco())
+    @staticmethod
+    def salvar():
+        Task.criar_tabela()
+        conn = sqlite3.connect(Task.caminho_banco())
         cursor = conn.cursor()
         cursor.execute('''
             INSERT INTO tarefas (titulo, descricao, data_criacao)
             VALUES (?, ?, ?)
-        ''', (self.titulo, self.descricao, self.data_criacao))
+        ''', (Task.titulo, Task.descricao, Task.data_criacao))
         conn.commit()
         conn.close()
 
-    def verificar_se_tabela_existe(self):
-        conn = sqlite3.connect(self.caminho_banco())
+    @staticmethod
+    def verificar_se_tabela_existe():
+        conn = sqlite3.connect(Task.caminho_banco())
         cursor = conn.cursor()
         cursor.execute("""
             SELECT name FROM sqlite_master 
@@ -51,35 +56,38 @@ class Task:
             conn.close()
             return True
         else:
-            self.criar_tabela()
+            Task.criar_tabela()
             conn.close()
             return False
 
-    def deletar(self, id):
-        conn = sqlite3.connect(self.caminho_banco())
+    @staticmethod
+    def deletar(Task, id):
+        conn = sqlite3.connect(Task.caminho_banco())
         cursor = conn.cursor()
         cursor.execute("DELETE FROM tarefas WHERE id = ?", (id,))
         conn.commit()
         conn.close()
 
-    def atualizar(self):
-        if self.id is None:
+    @staticmethod
+    def atualizar():
+        if Task.id is None:
             print("Tarefa precisa de um ID para atualizar")
             return
 
-        conn = sqlite3.connect(self.caminho_banco())
+        conn = sqlite3.connect(Task.caminho_banco())
         cursor = conn.cursor()
         cursor.execute('''
             UPDATE tarefas 
             SET titulo = ?, descricao = ?
             WHERE id = ?
-        ''', (self.titulo, self.descricao, self.id))
+        ''', (Task.titulo, Task.descricao, Task.id))
 
         conn.commit()
         conn.close()
 
-    def listar(self):
-        conn = sqlite3.connect(self.caminho_banco())
+    @staticmethod
+    def listar():
+        conn = sqlite3.connect(Task.caminho_banco())
         cursor = conn.cursor()
         cursor.execute('''
             SELECT id, titulo, descricao, data_criacao
--- app/services/task_services.py
+++ app/services/task_services.py
@@ -7,15 +7,12 @@ class TaskServices():
         return nova_tarefa
 
     def listar_tarefas(self):
-        task = Task("", "")
         return task.listar()
     
     def verificar_se_tabela_existe(self):
-        task = Task("", "")
         return task.verificar_se_tabela_existe()
     
     def deletar_tarefas(self, id):
-        task = Task("", "")
         return task.deletar(id)
     
     def atualizar(self, id, titulo, descricao):
